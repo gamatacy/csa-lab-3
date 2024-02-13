9d8491b8fa10403ce2c0f011ec3f7cc86b543d1f
Ivan Voronin <i.voronin@yadro.com>
2024-02-13 18:23:07 +0300

    fix tests

--- tests/test_golden.py
+++ tests/test_golden.py
@@ -3,7 +3,6 @@ import pytest
 from compiler.compiler import compile
 from emu.bl import parse_yaml_data,parse_yaml_input,parse_yaml_instructions
 from emu.soc import Soc
-import filecmp
 
 @pytest.mark.golden_test("golden/*.yaml")
 def test_golden(golden, caplog):
@@ -25,15 +24,12 @@ def test_golden(golden, caplog):
     buff = parse_yaml_input(yaml_data['input'])
     start_addr = int(yaml_data['start_addr'])
     
-    dump_name = golden["dump"]
-    idx = 0
-    f = open(dump_name, "r")
-    for line in f.readlines():
-        print(line)
-        idx += 1
-        if (idx == 15): break
-
     soc = Soc()
     soc.run(start_addr, instructions, data, buff)
 
-    assert filecmp.cmp(dump_name, "emu.dump")
+    a = open(golden["dump"],"r")
+    b = open("emu.dump","r")
+
+    assert a.read() == b.read()
+
+    # assert filecmp.cmp(dump_name, "emu.dump")
